3e23978d4ed85c61faa5856923fc9e25fed9b4c9
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-18 13:28:53 -0600

    refactor disable clear button event

--- javascripts/disabled.js
+++ javascripts/disabled.js
@@ -1,11 +1,22 @@
 "use strict";
 
 let messageController = require("./appData");
+let objArr = require("./outputToDom");
 
-module.exports.toggleDisabled = () => {
-    if (messageController.getMessages().length === 0) {
+// module.exports.toggleDisabled = () => {
+//     if (messageController.getMessages().length === 0) {
+//         document.getElementById("clear").disabled = true;
+//     } else {
+//         document.getElementById("clear").disabled = false;    
+//     }
+// };
+
+module.exports.Disabled = (array) => {
+    if (array.length === 0) {
         document.getElementById("clear").disabled = true;
-    } else {
-        document.getElementById("clear").disabled = false;    
+    // } else {
+    //     document.getElementById("clear").disabled = false;    
     }
 };
+
+    
