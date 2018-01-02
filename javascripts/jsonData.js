9774cad04acd16f896fdda3104258f36553175fa
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-02 14:34:00 -0600

    ajax call replace xhr request

--- javascripts/jsonData.js
+++ javascripts/jsonData.js
@@ -4,17 +4,45 @@ let messageController = require("./appData");
 let output = require ("./outputToDom");
 
 module.exports.getjsonData =() => {
-    let preMsg = null;
-    const msgReq = new XMLHttpRequest();
-    msgReq.addEventListener("load", parseMsg);
-    msgReq.open("GET", "messages.json");
-    msgReq.send(); 
+    $.ajax({
+        url:"messages.json",
+    }).done(
+            (data)=>{
+                 console.log(data);
+                parseMsg(data);
+            }
+        );
+    
+    // let preMsg = null;
+    // const msgReq = new XMLHttpRequest();
+    // msgReq.addEventListener("load", parseMsg);
+    // msgReq.open("GET", "messages.json");
+    // msgReq.send(); 
 };
 
-const parseMsg = () => {
-    const msgData = JSON.parse(event.target.responseText).messages;
-    for (let i = 0; i < msgData.length; i++) {
-        let arrayWithJson = messageController.addNewMessage(msgData[i]); 
+const parseMsg = (ajaxData) => {
+    // const msgData = JSON.parse(event.target.responseText).messages;
+    console.log("ajax data", ajaxData);
+    for (let i = 0; i < ajaxData.messages.length; i++) {
+        let arrayWithJson = messageController.addNewMessage(ajaxData.messages[i]); 
         output.updateDom(arrayWithJson);
     }      
 };
+
+// $.ajax({
+//     url: "songs.json"
+//   })
+//   .done(executeAfterAjax)
+//   .fail(ifBork);
+  
+//   function ifBork(data) {
+//     console.log('Ooops', data);
+//   }
+  
+//   function executeAfterAjax(data) {
+//     console.log(data.songs);
+//     let songs = data.songs;
+//     $(songs).each( (index, song) => {
+//       $("#song-list").append(`<li>${song.title}</li>`)
+//     });
+//   }
